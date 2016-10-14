a4bc8749d9ffa51a85f1372f2532f8830c344f03
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-10-14 10:34:49 +0100

    minor refactoring

--- src/main/java/com/snovelli/model/RedirectSpecification.java
+++ src/main/java/com/snovelli/model/RedirectSpecification.java
@@ -1,20 +1,10 @@
 package com.snovelli.model;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-/**
- * Created by Salvatore on 24/04/2016.
- */
 public final class RedirectSpecification {
 
     private final String sourceURI;
     private final String expectedDestination;
 
-
-//    public RedirectSpecification(String sourceURI, String expectedDestinationURI) throws URISyntaxException {
-//        this(new URI(sourceURI.trim()), new URI(expectedDestinationURI.trim()));
-//    }
 
     public RedirectSpecification(String sourceURI, String expectedDestination) {
         this.sourceURI = sourceURI;
