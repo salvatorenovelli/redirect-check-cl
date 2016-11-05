751379e1ee51a59bac210843fe41c1c31f0f89b2
snovelli <salvatore.novelli@gmail.com>
2016-11-05 13:33:01 +0000

    Refactored CVS output in separate class

--- src/main/java/com/github/salvatorenovelli/Application.java
+++ src/main/java/com/github/salvatorenovelli/Application.java
@@ -12,12 +12,9 @@ import com.github.salvatorenovelli.seo.redirect.RedirectSpecificationCSVReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -28,20 +25,19 @@ public class Application {
     private static final int NUM_WORKERS = 50;
     private final RedirectChainAnalyser redirectChainAnalyser;
     private final String filename;
-    private final FileWriter csvOutput;
-    private TextProgressBar progressBar;
+    private final RedirectCheckResponseCsvSerializer csvWriter;
 
+    private TextProgressBar progressBar;
 
     private Application(String sourceFilename) throws IOException {
         this.filename = sourceFilename;
 
         String outFileName = sourceFilename + "_out.csv";
         try {
-            this.csvOutput = new FileWriter(new File(outFileName));
-            csvOutput.append(csvHeader() + "\n");
+            this.csvWriter = new RedirectCheckResponseCsvSerializer(outFileName);
             this.redirectChainAnalyser = new DefaultRedirectChainAnalyser(new DefaultHttpConnectorFactory());
         } catch (FileNotFoundException e) {
-            throw new RuntimeException("Unable to create output file: " + outFileName+". File may be busy or write protected.");
+            throw new RuntimeException("Unable to create output file: " + outFileName + ". File may be busy or write protected.");
         }
 
     }
@@ -96,9 +92,7 @@ public class Application {
 
     private void runAnalysis() throws IOException, ExecutionException, InterruptedException {
         List<RedirectCheckResponse> responses = analyseRedirectsInCSV(filename);
-        responses.forEach(this::tocsv);
-        csvOutput.close();
-
+        csvWriter.writeall(responses);
     }
 
     private List<RedirectCheckResponse> analyseRedirectsInCSV(String filePath) throws IOException, ExecutionException, InterruptedException {
@@ -119,33 +113,4 @@ public class Application {
     }
 
 
-    private String csvHeader() {
-        return "SourceURI, RESULT, ResultReason, Expected URI, Actual URI, Last HTTP Status";
-    }
-
-    private void tocsv(RedirectCheckResponse cr) {
-        List<String> fields = Arrays.asList(
-                cr.getSourceURI(),
-                cr.getStatus().toString(),
-                cr.getStatusMessage(),
-                cr.getExpectedDestinationURI(),
-                cr.getActualDestinationURI() != null ? cr.getActualDestinationURI() : "n/a",
-                cr.getLastHttpStatus() != -1 ? "" + cr.getLastHttpStatus() : "n/a"
-        );
-
-        fields.forEach(this::appendToCSVOutput);
-        appendToCSVFile("\n");
-    }
-
-    private void appendToCSVOutput(String field) {
-        appendToCSVFile(field + ",");
-    }
-
-    private void appendToCSVFile(String field) {
-        try {
-            csvOutput.append(field);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
---
+++ src/main/java/com/github/salvatorenovelli/RedirectCheckResponseCsvSerializer.java
@@ -0,0 +1,61 @@
+package com.github.salvatorenovelli;
+
+import com.github.salvatorenovelli.model.RedirectCheckResponse;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by Salvatore on 05/11/2016.
+ */
+public class RedirectCheckResponseCsvSerializer {
+
+    private final FileWriter csvOutput;
+
+    public RedirectCheckResponseCsvSerializer(String outFileName) throws IOException {
+        this.csvOutput = new FileWriter(new File(outFileName));
+        csvOutput.append(csvHeader()).append("\n");
+    }
+
+    public void writeall(Collection<RedirectCheckResponse> responses) throws IOException {
+        try {
+            responses.forEach(this::tocsv);
+        } finally {
+            csvOutput.close();
+        }
+    }
+
+    private String csvHeader() {
+        return "SourceURI, RESULT, ResultReason, Expected URI, Actual URI, Last HTTP Status";
+    }
+
+    private void tocsv(RedirectCheckResponse cr) {
+        List<String> fields = Arrays.asList(
+                cr.getSourceURI(),
+                cr.getStatus().toString(),
+                cr.getStatusMessage(),
+                cr.getExpectedDestinationURI(),
+                cr.getActualDestinationURI() != null ? cr.getActualDestinationURI() : "n/a",
+                cr.getLastHttpStatus() != -1 ? "" + cr.getLastHttpStatus() : "n/a"
+        );
+
+        fields.forEach(this::appendToCSVOutput);
+        appendToCSVFile("\n");
+    }
+
+    private void appendToCSVOutput(String field) {
+        appendToCSVFile(field + ",");
+    }
+
+    private void appendToCSVFile(String field) {
+        try {
+            csvOutput.append(field);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
