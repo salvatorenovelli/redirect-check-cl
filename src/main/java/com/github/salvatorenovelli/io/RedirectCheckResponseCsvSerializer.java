97cfa20b06d915b04449a9534e0f824635571e6f
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-12-12 10:59:18 +0000

    Preparing the output serialization to include bad specs

--- src/main/java/com/github/salvatorenovelli/io/RedirectCheckResponseCsvSerializer.java
+++ src/main/java/com/github/salvatorenovelli/io/RedirectCheckResponseCsvSerializer.java
@@ -1,24 +1,37 @@
 package com.github.salvatorenovelli.io;
 
+import com.github.salvatorenovelli.model.InvalidRedirectSpecification;
 import com.github.salvatorenovelli.model.RedirectCheckResponse;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class RedirectCheckResponseCsvSerializer {
 
     private final FileWriter csvOutput;
+    private SortedSet<ResponseWrapper> responses = new TreeSet<>(Comparator.comparingInt(ResponseWrapper::getLineNumber));
 
     public RedirectCheckResponseCsvSerializer(String outFileName) throws IOException {
-        this.csvOutput = new FileWriter(new File(outFileName));
+        csvOutput = new FileWriter(new File(outFileName));
         csvOutput.append(csvHeader()).append("\n");
     }
 
-    public void writeall(Collection<RedirectCheckResponse> responses) throws IOException {
+    public void addResponses(List<RedirectCheckResponse> responses) throws IOException {
+        this.responses.addAll(responses.stream().map(ResponseWrapper::new).collect(Collectors.toList()));
+    }
+
+    public void addInvalidSpecs(List<InvalidRedirectSpecification> invalidSpec) {
+        responses.addAll(invalidSpec.stream().map(ResponseWrapper::new).collect(Collectors.toList()));
+    }
+
+    public void write() throws IOException {
         try {
             responses.forEach(this::tocsv);
         } finally {
@@ -27,19 +40,11 @@ public class RedirectCheckResponseCsvSerializer {
     }
 
     private String csvHeader() {
-        return "SourceURI, RESULT, ResultReason, Expected URI, Actual URI, Last HTTP Status";
+        return "Line #, SourceURI, RESULT, ResultReason, Expected URI, Actual URI, Last HTTP Status";
     }
 
-    private void tocsv(RedirectCheckResponse cr) {
-        List<String> fields = Arrays.asList(
-                cr.getSourceURI(),
-                cr.getStatus().toString(),
-                cr.getStatusMessage(),
-                cr.getExpectedDestinationURI(),
-                cr.getActualDestinationURI() != null ? cr.getActualDestinationURI() : "n/a",
-                cr.getLastHttpStatus() != -1 ? "" + cr.getLastHttpStatus() : "n/a"
-        );
-
+    private void tocsv(ResponseWrapper cr) {
+        List<String> fields = Arrays.asList(String.valueOf(cr.lineNumber), cr.sourceURI, cr.result, cr.reason, cr.expectedURI, cr.actualURI, cr.lastHTTPStatus);
         fields.forEach(this::appendToCSVOutput);
         appendToCSVFile("\n");
     }
@@ -53,6 +58,44 @@ public class RedirectCheckResponseCsvSerializer {
             csvOutput.append(field);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static class ResponseWrapper {
+
+        private final String result;
+        private final String reason;
+        private final String expectedURI;
+        private final String actualURI;
+        private final String lastHTTPStatus;
+        private final String sourceURI;
+        private final int lineNumber;
+
+        private ResponseWrapper(RedirectCheckResponse cr) {
+            this(cr.getRequestLineNumber(), cr.getSourceURI(),
+                    cr.getStatus().toString(),
+                    cr.getStatusMessage(),
+                    cr.getExpectedDestinationURI(),
+                    cr.getActualDestinationURI() != null ? cr.getActualDestinationURI() : "n/a",
+                    cr.getLastHttpStatus() != -1 ? "" + cr.getLastHttpStatus() : "n/a");
+        }
+
+        public ResponseWrapper(InvalidRedirectSpecification cr) {
+            this(cr.getLineNumber(), "n/a", RedirectCheckResponse.Status.FAILURE.name(), cr.getErrorMessage(), "", "", "");
+        }
+
+        private ResponseWrapper(int lineNumber, String sourceURI, String result, String reason, String expectedURI, String actualURI, String lastHTTPStatus) {
+            this.lineNumber = lineNumber;
+            this.sourceURI = sourceURI;
+            this.result = result;
+            this.reason = reason;
+            this.expectedURI = expectedURI;
+            this.actualURI = actualURI;
+            this.lastHTTPStatus = lastHTTPStatus;
+        }
+
+        public int getLineNumber() {
+            return lineNumber;
         }
     }
 }
