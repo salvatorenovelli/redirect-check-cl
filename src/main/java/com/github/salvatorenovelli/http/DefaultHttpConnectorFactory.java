8e7379d6887c882c221f2008cbf9d31431e301be
snovelli <salvatore.novelli@gmail.com>
2017-02-26 14:33:26 +0000

    Using latest stable version of api

--- src/main/java/com/github/salvatorenovelli/http/DefaultHttpConnectorFactory.java
+++ src/main/java/com/github/salvatorenovelli/http/DefaultHttpConnectorFactory.java
@@ -3,11 +3,9 @@ package com.github.salvatorenovelli.http;
 
 import com.github.salvatorenovelli.redirectcheck.domain.HttpRequest;
 import com.github.salvatorenovelli.redirectcheck.domain.HttpRequestFactory;
-
+import com.github.salvatorenovelli.redirectcheck.http.DefaultConnectionFactory;
 import com.github.salvatorenovelli.redirectcheck.http.HttpGetRequest;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-
 
 import java.net.URI;
 
@@ -17,6 +15,6 @@ public class DefaultHttpConnectorFactory implements HttpRequestFactory {
 
     @Override
     public HttpRequest createRequest(URI httpURI) {
-        return new HttpGetRequest(httpURI);
+        return new HttpGetRequest(httpURI, new DefaultConnectionFactory());
     }
 }
